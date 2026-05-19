55202f1185f3877aa970092de671c412bd89f913
Luke <sensiblecat010@gmail.com>
2026-05-19 15:11:35 +0100

    First implementation of main page

--- src/main/java/com/f1weather/App.java
+++ src/main/java/com/f1weather/App.java
@@ -1,9 +1,12 @@
 package com.f1weather;
 
+import com.sun.tools.javac.Main;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class App extends Application {
 
@@ -11,9 +14,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+
         scene = new Scene(new MainPage(), 400, 866);
-        
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         stage.setScene(scene);
         stage.setTitle("F1 Weather App");
         stage.show();
--- src/main/java/com/f1weather/MainPage.java
+++ src/main/java/com/f1weather/MainPage.java
@@ -1,17 +1,72 @@
 package com.f1weather;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.geometry.Insets;
+import javafx.scene.text.Font;
 
 public class MainPage extends StackPane {
     
     public MainPage() {
-        
-        var label = new Label("Welcome to the F1 Weather App!");
-        label.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        
-        this.getChildren().add(label);
-        
-        
+//
+//        var label = new Label("Welcome to the F1 Weather App!");
+//        label.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+//
+//        this.getChildren().add(label);
+//
+
+        // TODO: Fix font
+        String fontUrl = "https://fonts.gstatic.com/s/fasterone/v19/H4clBX6Ar_vka0DcxO46pE7Wiw.ttf";
+        Font fasterOne = Font.loadFont(fontUrl, 24);
+
+
+        Label header = new Label("Race Selector");
+        header.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-background-color: #ddd;");
+        if (fasterOne != null) {
+            header.setFont(fasterOne);
+        }
+        header.setMaxWidth(Double.MAX_VALUE);
+        header.setPadding(new Insets(10));
+
+
+         // TODO:
+        // get race list here
+        // then pass in nicely
+        VBox contentBox = new VBox(10); // 10px spacing between items
+        for (int i = 1; i <= 50; i++) {
+            Button b = new Button();
+            Label nameLabel = new Label("FILLER NAME");
+            //nameLabel.setStyle("-fx-background-color: ")
+            HBox innerLayout = new HBox(10);
+            HBox dateBox = new HBox(10);
+            dateBox.setStyle("-fx-background-color: #2A2A2E; -fx-alignment: centre-right");
+            //dateBox.setAlignment();
+            Label date = new Label("FILLER DATE");
+            date.setStyle("-fx-background-color: #FFFFFF;");
+            dateBox.getChildren().addAll(date);
+            innerLayout.getChildren().addAll(nameLabel, dateBox);
+            b.setMaxWidth(Double.MAX_VALUE);
+            b.getStyleClass().add("main-page-button");
+            b.setGraphic(innerLayout);
+            contentBox.getChildren().add(b);
+        }
+
+        ScrollPane scrollPane = new ScrollPane(contentBox);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPadding(new Insets(0, 20, 0, 20));
+
+
+
+        VBox rootLayout = new VBox(scrollPane); // ScrollPane in root box
+        rootLayout.getChildren().add(0, header);
+
+        this.getChildren().add(rootLayout);
+
     }
 }
